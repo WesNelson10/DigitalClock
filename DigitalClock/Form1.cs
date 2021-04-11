310b95d4043c6242e2844486ff9418449956391e
Wes Nelson <WesNelson10@outlook.com>
2021-04-10 23:30:34 -0500

    Messed around with the layout.

--- DigitalClock/Form1.cs
+++ DigitalClock/Form1.cs
@@ -19,11 +19,13 @@ namespace DigitalClock
 
         private void timer_Tick(object sender, EventArgs e)
         {
+            // Display current time data on the labels as strings.
             lbl24.Text = DateTime.Now.ToString("HH:mm");
             lblSecond.Text = DateTime.Now.ToString("ss");
-            lblDate.Text = DateTime.Now.ToString("MMMMMMMM dd yyyy");
+            lblDate.Text = DateTime.Now.ToString("MMMMMMMM dd yyyy"); // I think this will work for all months. It should.
             lblDay.Text = DateTime.Now.ToString("dddd");
             lblSecond.Location = new Point(lbl24.Location.X + lbl24.Width-5, lblSecond.Location.Y);
+            // The icon flag is Thai, my father was born there.
         }
 
         private void Form1_Load(object sender, EventArgs e)
