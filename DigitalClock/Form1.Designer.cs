2814c334abb55bccc2c4b7b9243c58adaf93c912
Wes Nelson <WesNelson10@outlook.com>
2021-04-10 22:27:19 -0500

    Initial Commit
    I realized I don't know how to describe my commits.
    
    Here I built a digital clock using DateTime.
    
    I need to change it to 12 hour format, but I'm going to go work on something else.

--- DigitalClock/Form1.Designer.cs
+++ DigitalClock/Form1.Designer.cs
@@ -29,12 +29,88 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lbl24 = new System.Windows.Forms.Label();
+            this.lblSecond = new System.Windows.Forms.Label();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbl24
+            // 
+            this.lbl24.AutoSize = true;
+            this.lbl24.Font = new System.Drawing.Font("Papyrus", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl24.ForeColor = System.Drawing.Color.White;
+            this.lbl24.Location = new System.Drawing.Point(58, 35);
+            this.lbl24.Name = "lbl24";
+            this.lbl24.Size = new System.Drawing.Size(387, 189);
+            this.lbl24.TabIndex = 0;
+            this.lbl24.Text = "22:22";
+            // 
+            // lblSecond
+            // 
+            this.lblSecond.AutoSize = true;
+            this.lblSecond.Font = new System.Drawing.Font("Papyrus", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecond.ForeColor = System.Drawing.Color.White;
+            this.lblSecond.Location = new System.Drawing.Point(407, 112);
+            this.lblSecond.Name = "lblSecond";
+            this.lblSecond.Size = new System.Drawing.Size(89, 76);
+            this.lblSecond.TabIndex = 1;
+            this.lblSecond.Text = "22";
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.Font = new System.Drawing.Font("Papyrus", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDay.ForeColor = System.Drawing.Color.White;
+            this.lblDay.Location = new System.Drawing.Point(526, 323);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(299, 94);
+            this.lblDay.TabIndex = 2;
+            this.lblDay.Text = "Saturday";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Papyrus", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.ForeColor = System.Drawing.Color.White;
+            this.lblDate.Location = new System.Drawing.Point(41, 323);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(404, 94);
+            this.lblDate.TabIndex = 3;
+            this.lblDate.Text = "June 25 2016";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(860, 473);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.lblSecond);
+            this.Controls.Add(this.lbl24);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl24;
+        private System.Windows.Forms.Label lblSecond;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- DigitalClock/Form1.cs
+++ DigitalClock/Form1.cs
@@ -16,5 +16,19 @@ namespace DigitalClock
         {
             InitializeComponent();
         }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            lbl24.Text = DateTime.Now.ToString("HH:mm");
+            lblSecond.Text = DateTime.Now.ToString("ss");
+            lblDate.Text = DateTime.Now.ToString("MMMMMMMM dd yyyy");
+            lblDay.Text = DateTime.Now.ToString("dddd");
+            lblSecond.Location = new Point(lbl24.Location.X + lbl24.Width-5, lblSecond.Location.Y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
     }
 }
